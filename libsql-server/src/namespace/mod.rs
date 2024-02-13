cf85202d3ee4995e8d96b3017673bd4cfd59f3c8
ad hoc <postma.marin@protonmail.com>
2024-02-13 14:06:06 +0100

    replica wait when snapshot not found

--- libsql-server/src/namespace/mod.rs
+++ libsql-server/src/namespace/mod.rs
@@ -944,6 +944,7 @@ impl Namespace<ReplicaDatabase> {
                         // we reset the client state.
                         replicator.client_mut().reset_token();
                     }
+                    Error::SnapshotPending => unreachable!(),
                 }
             }
         });
